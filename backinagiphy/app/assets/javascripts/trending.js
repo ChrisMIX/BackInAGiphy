a0855342875e166c95fddcd3ac3b44a1235577cc
Chris Miklius <cmiklius@gmail.com>
2018-11-10 22:30:51 -0600

    create basic search function

--- backinagiphy/app/assets/javascripts/trending.js
+++ backinagiphy/app/assets/javascripts/trending.js
@@ -1,4 +1,7 @@
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=CAj2qjW87gmnCf2At3tTexta2bhn3dLQ&limit=30");
+var url = "http://api.giphy.com/v1/gifs/search?q="
+var input = $("#searchtext").val();
+var api = "&api_key=CAj2qjW87gmnCf2At3tTexta2bhn3dLQ&limit=30"
+var xhr = $.get(url + input + api);
 
 xhr.done(function(response) { console.log("success got data", response);
 
@@ -6,8 +9,10 @@ var gifs = response.data;
 
 $('.searchthemgifs').on("submit", function(e){
   e.preventDefault();
-  console.log(this);
+
+  for (i in gifs)
+  {
+  $('.gifsearchresults').append("<img src='"+gifs[i].images.original.url+"'/>")
+  }
 })
-
-
 });
