59fd4c489c8d48223d52a254f6ff5cd2be1f7270
Chris Miklius <cmiklius@gmail.com>
2018-11-12 17:20:21 -0600

    finish search page and add styling

--- backinagiphy/app/assets/javascripts/trending.js
+++ backinagiphy/app/assets/javascripts/trending.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-$('.searchthemgifs').on("submit", function(e){
-  e.preventDefault();
+  $('.searchthemgifs').on("submit", function(e){
+    e.preventDefault();
 
-  var url = "http://api.giphy.com/v1/gifs/search?q="
-  var input = $("#searchtext").val();
-  var api = "&api_key=CAj2qjW87gmnCf2At3tTexta2bhn3dLQ&limit=30"
-  var gifs = $.get(url + input + api);
-  var response = gifs.status;
-  console.log(gifs);
-  console.log(response);
+    var url = "http://api.giphy.com/v1/gifs/search?q="
+    var input = $("#searchtext").val();
+    var api = "&api_key=CAj2qjW87gmnCf2At3tTexta2bhn3dLQ&limit=30"
+    var gifs = $.get(url + input + api);
 
-  for (i in gifs)
-  {
-  $('.gifsearchresults').append("<img src='"+gifs[i].images.original.url+"'/>")
-  }
-})
+    gifs.done(function(response) {
+      $(".gifsearchresults").html("");
+      giffs = response.data;
+      for (i in giffs)
+      {
+        $('.gifsearchresults').append("<img src='"+giffs[i].images.original.url+"'/>")
+      }
+    });
+
+
+  })
 });
