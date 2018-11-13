e4bd22f6d8cf648c4fb1f62f95dc331ab1a72628
Chris Miklius <cmiklius@gmail.com>
2018-11-12 20:11:19 -0600

    create trending page

--- backinagiphy/app/assets/javascripts/trending.js
+++ backinagiphy/app/assets/javascripts/trending.js
@@ -17,5 +17,18 @@ $(document).ready(function(){
     });
 
 
+
   })
 });
+
+var gifs2 = $.get("http://api.giphy.com/v1/gifs/trending?&api_key=CAj2qjW87gmnCf2At3tTexta2bhn3dLQ&limit=30");
+console.log(gifs2);
+    gifs2.done(function(response){
+      giffs2 = response.data
+      console.log(giffs2);
+      for (i in giffs2)
+      {
+        $('.trendingsearchresults').append("<img src='"+giffs2[i].images.original.url+"'/>")
+        console.log(giffs2[i].images.original.url);
+      }
+    });
